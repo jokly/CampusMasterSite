99d3a6da658daa8d82a306b43d4eb79f678c7847
Slasten Tikhon <slasten32@gmail.com>
2017-12-09 00:13:57 +1000

    Added status model to admin panel

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
 web: gunicorn campus_master_site.wsgi --log-file -
-
-make: python3 manage.py makemigrations
-
-mig: python3 manage.py migrate
--- campus_master/admin.py
+++ campus_master/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import User
-from .models import Complaint
+from .models import User, Complaint, Status
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Complaint)
+admin.site.register(Status)
